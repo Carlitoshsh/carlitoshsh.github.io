dd1c3421b6835dd8117f6cf9485c3311e23bc389
Carlos Gómez <caragomezher@unal.edu.co>
2016-05-25 17:33:24 -0500

    Update manager.js

--- states/manager.js
+++ states/manager.js
@@ -9,8 +9,11 @@
 var game = new Phaser.Game(640, 480, Phaser.CANVAS, 'game');
 
 // Se crean los estados del juego
+// Este contiene el inicio del juego, que inicia cuando el usuario presiona ENTER
+game.state.add('iniciar', inicio);
+// Al presionar enter lo dirige al siguiente estado
 game.state.add('juego', estudianteEnFinales);
 //game.state.add('fin', winState);
 
 // Se llama al primer estado
-game.state.start('juego');
+game.state.start('iniciar');
