5ce406b32ed8c2f0c6e60a43fa220e1dbc8b14cb
Carlitoshsh <caragomezher@unal.edu.co>
2018-04-01 13:57:38 -0500

    Added Material Design For Components

--- js/main.js
+++ js/main.js
@@ -1,30 +1,53 @@
 import angular from "angular";
+import mdcAutoInit from '@material/auto-init';
+
+import {
+  MDCComponent,
+  MDCFoundation
+} from '@material/base';
+
+import {MDCRipple} from '@material/ripple';
+
+import {
+  MDCToolbar,
+  MDCToolbarFoundation
+} from '@material/toolbar';
+
+
+const ripple = new MDCRipple(document.querySelector('.foo-button'));
+const toolbar = new MDCToolbar(document.querySelector(".mdc-toolbar"))
+toolbar.fixedAdjustElement = document.querySelector('.mdc-toolbar-fixed-adjust');
 
 angular.module('todoApp', [])
-  .controller('TodoListController', function() {
+  .controller('TodoListController', function () {
+
     var todoList = this;
+
     todoList.todos = [
-      {text:'learn AngularJS', done:true},
-      {text:'build an AngularJS app', done:false}];
- 
-    todoList.addTodo = function() {
-      todoList.todos.push({text:todoList.todoText, done:false});
+      { text: 'Aprender AngularJS', done: true },
+      { text: 'Usar Material Design for Components', done: true },
+      { text: 'Construir una aplicación con AngularJS', done: false }
+    ];
+
+    todoList.addTodo = function () {
+      todoList.todos.push({ text: todoList.todoText, done: false });
       todoList.todoText = '';
     };
- 
-    todoList.remaining = function() {
+
+    todoList.remaining = function () {
       var count = 0;
-      angular.forEach(todoList.todos, function(todo) {
+      angular.forEach(todoList.todos, function (todo) {
         count += todo.done ? 0 : 1;
       });
       return count;
     };
- 
-    todoList.archive = function() {
+
+    todoList.archive = function () {
       var oldTodos = todoList.todos;
       todoList.todos = [];
-      angular.forEach(oldTodos, function(todo) {
+      angular.forEach(oldTodos, function (todo) {
         if (!todo.done) todoList.todos.push(todo);
       });
     };
   });
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,50 @@
-module.exports = {
+module.exports = [{
     entry: './js/main.js',
     output: {
         filename: 'bundle.js'
     },
     module: {
-        rules: [
-            {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['env']
-                }
-              }
-            }
-        ]
+        loaders: [{
+          test: /\.js$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015']
+          }
+        }]
     },
-};
+}, {
+    entry: './css/main.scss',
+    output: {
+        // This is necessary for webpack to compile
+        // But we never use style-bundle.js
+        filename: 'style-bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'bundle.css',
+                    },
+                },
+                { loader: 'extract-loader' },
+                { loader: 'css-loader' },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                      importer: function(url, prev) {
+                        if(url.indexOf('@material') === 0) {
+                          var filePath = url.split('@material')[1];
+                          var nodeModulePath = `./node_modules/@material/${filePath}`;
+                          return { file: require('path').resolve(nodeModulePath) };
+                        }
+                        return { file: url };
+                      }
+                    }
+                  },
+            ]
+        }]
+    },
+}];
