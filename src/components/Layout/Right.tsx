7a1d378221d64814b41ed4269bd9ae152772207e
Carlos Gómez <caragomezher@unal.edu.co>
2022-07-06 08:22:57 -0500

    Testing...

--- src/components/Layout/Right.tsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components'
-
-export const Right = styled.div`
-    grid-column: 3 / 4
-`
--- src/components/Layout/index.ts
+++
@@ -1,6 +0,0 @@
-export { Footer } from './Footer'
-export { Header } from './Header'
-export { Left } from './Left'
-export { Right } from './Right'
-export { Main } from './Main'
-export { Page } from './Page'
